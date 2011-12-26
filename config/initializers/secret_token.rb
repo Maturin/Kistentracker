7a21ee985d4f3bbedaecbc1370958be589c4eb3b
U-TOMTEC\lbenner <lbenner@MobileMUC360.tomtec.org>
2011-12-26 11:41:50 +0100

    Initial checkin after creating Rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kistentracker::Application.config.secret_token = '052c556775f54c2b8f8aaa6ea8dd2847bbe47658a0ac5b284f37902cb2de632bda43d775399144556baece699ca6b50ebcbac0375cf348e1817fc54983911e05'
