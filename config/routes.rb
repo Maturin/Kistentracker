a064d293853802fc0707e98e4546a5f02631b6b8
Lars Benner <lars.benner@gmail.com>
2011-12-26 17:43:33 +0100

    Made overview default page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Kistentracker::Application.routes.draw do
+  resources :overview
+
   resources :craters
 
   resources :craterpayers
@@ -54,7 +56,7 @@ Kistentracker::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'overview#index'
 
   # See how all your routes lay out with "rake routes"
 
