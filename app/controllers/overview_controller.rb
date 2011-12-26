8b59490ea46792a98b9c7dac1c09325d18478f48
Lars Benner <lars.benner@gmail.com>
2011-12-26 19:34:09 +0100

    Show next craters to pay and last craters paid

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -2,11 +2,12 @@ class OverviewController < ApplicationController
   # GET /teammates
   # GET /teammates.json
   def index
-    @craters = Overview.all
+    @pend_craters = Overview.get_pending_craters 
+    @paid_craters = Overview.get_last_paid_craters 
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render :json => @craters }
+      #format.json { render :json => @craters }
     end
   end
 
--- app/models/overview.rb
+++ app/models/overview.rb
@@ -1,3 +1,20 @@
 class Overview < ActiveRecord::Base
   self.table_name = 'craters'
+
+  def Overview.get_pending_craters
+    return Overview.find_by_sql <<EOSQL
+SELECT craters.id, craterpayers.name, craters.reason, craters."date" 
+FROM craters, craterpayers 
+WHERE (craterpayers.id = craters.craterpayer_id) AND (craters.paid IS NULL);
+EOSQL
+  end
+
+  def Overview.get_last_paid_craters
+    return Overview.find_by_sql <<EOSQL
+SELECT craters.id, craterpayers.name, craters.reason, craters."paid" 
+FROM craters, craterpayers 
+WHERE (craterpayers.id = craters.craterpayer_id) AND (craters.paid IS NOT NULL);
+EOSQL
+  end
+
 end
