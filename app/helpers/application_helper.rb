176e90c882d77b01a289e15aa94473888931a750
Lars Benner <lars.benner@gmail.com>
2012-01-08 00:48:02 +0100

    Made menu application wide.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def logged_in?
+    return !session[:user_id].nil?
+  end
+
+  def admin_priveledge?
+    return (!session[:user_id].nil?) && (session[:user_id] == 1)
+  end
+
+  def crate_priveledge?
+    return (!session[:user_id].nil?) && (session[:user_id] == 1)
+  end
+
 end
--- app/helpers/overviews_helper.rb
+++ app/helpers/overviews_helper.rb
@@ -1,5 +1,2 @@
 module OverviewsHelper
-  def logged_in?
-    return !session[:user_id].nil?
-  end
 end
