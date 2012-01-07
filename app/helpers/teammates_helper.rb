b40afafdc1db5dde0f24c67d2a94617f75c681f0
Lars Benner <lars.benner@gmail.com>
2012-01-07 17:34:15 +0100

    Added callto link and changed heading.

--- app/helpers/teammates_helper.rb
+++ app/helpers/teammates_helper.rb
@@ -1,2 +1,10 @@
 module TeammatesHelper
+  def call_to(n)
+    link = ""
+    n = n.to_s
+    if n.size > 0
+      link = "<a href=\"callto:#{n}\">#{n}</a>".html_safe
+    end
+    return link
+  end
 end
