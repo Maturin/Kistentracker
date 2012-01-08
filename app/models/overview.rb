7b4d9675a69e4ac31be0ca3e8e9c4ceaea5e7ad4
Lars Benner <lars.benner@gmail.com>
2012-01-08 16:26:01 +0100

    Fix SQL statement

--- app/models/overview.rb
+++ app/models/overview.rb
@@ -5,8 +5,8 @@ class Overview < ActiveRecord::Base
     return Overview.find_by_sql <<EOSQL
 SELECT crates.id, cratepayers.name, crates.reason, crates.date, crates.crate_type, crate_priorities.description
 FROM crates, cratepayers, crate_priorities
-WHERE (crates.paid IS NULL) AND (cratepayers.id = crates.cratepayer_id) AND (crates.crate_priority_value == crate_priorities.priority);
-ORDER BY crates.crate_priority DESC, crates.date ASC
+WHERE (crates.paid IS NULL) AND (cratepayers.id = crates.cratepayer_id) AND (crates.crate_priority_value = crate_priorities.priority)
+ORDER BY crates.crate_priority_value DESC, crates.date ASC
 EOSQL
   end
 
@@ -14,8 +14,8 @@ EOSQL
     return Overview.find_by_sql <<EOSQL
 SELECT crates.id, cratepayers.name, crates.reason, crates.paid, crates.crate_type, crate_priorities.description
 FROM crates, cratepayers, crate_priorities
-WHERE (crates.paid IS NOT NULL) AND (cratepayers.id = crates.cratepayer_id) AND (crates.crate_priority_value == crate_priorities.priority);
-ORDER BY crates.crate_priority DESC, crates.date ASC
+WHERE (crates.paid IS NOT NULL) AND (cratepayers.id = crates.cratepayer_id) AND (crates.crate_priority_value = crate_priorities.priority)
+ORDER BY crates.crate_priority_value DESC, crates.date ASC
 EOSQL
   end
 
